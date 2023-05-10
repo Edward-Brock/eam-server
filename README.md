93244044bd9c263cfa4cf382bbb1a91b13328e40
DESKTOP-4DBQPSO\14164 <1416477069@qq.com>
2023-05-10 09:41:51 +0800

    添加 Cors 跨域 配置 TypeOrm / mysql2 添加 .env.example 数据库配置示例 添加 README.md 内容

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ EAM（企业资产管理系统）服务端使用[Nest.js](https://nestjs.com/)�
 
 搭建轻量型企业资产管理系统，通过使用设备自带的图像捕捉工具或外置图像捕捉工具，扫描预制生成、固定生成的二维码、一维码或其他类型的资产标签，显示当前资产内容。
 
+## 使用技术
+
+- [TypeORM](https://typeorm.io/)
+- [Node MySQL 2](https://github.com/sidorares/node-mysql2)
+
 ## LICENSE
 
 [GNU General Public License v3.0](LICENSE)
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(express.urlencoded({ extended: true }));
+  app.enableCors();
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('SERVER_PORT');
+  await app.listen(port);
+  console.log(`
+  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  EAM Server 服务已启动：http://localhost:${port}
+  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  `);
 }
 bootstrap();
