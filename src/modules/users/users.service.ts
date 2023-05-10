bfdc4285b9fd30e79a4c9bb358c46bbd38100d50
DESKTOP-4DBQPSO\14164 <1416477069@qq.com>
2023-05-10 13:27:17 +0800

    更改 Users 查询条件

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,7 +4,6 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { Repository } from 'typeorm';
-import { log } from 'console';
 
 @Injectable()
 export class UsersService {
@@ -14,6 +13,7 @@ export class UsersService {
   ) { }
 
   create(createUserDto: CreateUserDto) {
+    createUserDto.create_time = createUserDto.update_time = new Date();
     return this.userRepository.save(createUserDto);
   }
 
@@ -21,16 +21,30 @@ export class UsersService {
     return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return this.userRepository.findOneBy({ user_id: id });
+  findOne(id: string) {
+    return this.userRepository.findOneBy({ openid: id });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return this.userRepository.update(id, updateUserDto);
   }
 
-  async remove(id: number) {
+  async remove(id: string) {
     let del_info = await this.findOne(id);
+    /**
+     * 使用软删除，对del_flag进行标记
+     * 0-false（未删除）；1-true（已删除）；
+     * 对将要删除的用户进行判断，如果manage_flag为真则为主管理用户，拒绝删除
+     * 如果为普通用户执行软删除操作，对del_flag进行标记，将delete_time置为当前时间
+     */
+    if (del_info.manage_flag) {
+      return {
+        code: 417,
+        state: 'error',
+        message: '具有管理权限的主管理用户无法删除'
+      }
+    }
+    del_info.delete_time = new Date();
     del_info.del_flag = true;
     return this.userRepository.update(id, del_info)
   }
